062882679d4c6ee11987bd0021c0bac70184eb15
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-29 21:48:23 +0300

    It will set uop the configuration in ini t file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from .config import DevConfig
 
 #Initializing the application
 app = Flask(__name__)
+#Setting up the configuration
+app.config.from_object(DevConfig)
 
 from . import views
