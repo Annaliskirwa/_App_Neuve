80832fa9b6450ae65008cf34bd024ed5309d62ea
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-29 22:37:58 +0300

    It will make an API call to the popular movies category

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #Views
 @app.route('/')
@@ -7,8 +8,8 @@ def index():
     '''
     View root page function that returns the root and its data
     '''
-    message = "Hello world from me to you"
-    return render_template('index.html', message=message)
+    popular_movies = get_movies('popular')
+    return render_template('index.html', popular=popular_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
