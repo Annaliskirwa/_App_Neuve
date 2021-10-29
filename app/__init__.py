b752a5f4ea899d21d6a9579aeb2713f156bb442f
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-29 22:12:39 +0300

    Storing API keys

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from .config import DevConfig
 
 #Initializing the application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 #Setting up the configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from . import views
