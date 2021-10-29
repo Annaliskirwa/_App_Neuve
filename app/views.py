5be527d43f7d19a8807dea01a3b697f5fb50be39
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-29 21:40:43 +0300

    It will return the movie id as an integer

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def index():
     message = "Hello world from me to you"
     return render_template('index.html', message=message)
 
-@app.route('/movie/<movie_id>')
+@app.route('/movie/<int:movie_id>')
 def movie(movie_id):
 
     '''
