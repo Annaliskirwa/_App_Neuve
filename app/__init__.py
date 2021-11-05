69857d05585e2d90401893cbdecbe7cef8978729
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-05 03:35:43 +0300

    Simple review editor

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from config import config_options
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
-
+from flask_simplemde import SimpleMDE
 
 
 login_manager = LoginManager()
@@ -17,10 +17,13 @@ db = SQLAlchemy()
 
 mail = Mail()
 
+simple = SimpleMDE()
+
 def create_app(config_name):
     app = Flask(__name__)
     #........
     mail.init_app(app)
+    simple.init_app(app)
 
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
