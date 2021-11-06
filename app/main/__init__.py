8ef2001278d4e60a59619836a35d559ce5310689
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-06 23:08:27 +0300

    Final files

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
-main = Blueprint('main',__name__) # blueprint takes in argument main as the name of the blueprint and __name__ to finds the location of the blueprint.
-from . import views,error
+
+main = Blueprint('main',__name__)
+
+from . import views,errors
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MOVIE_API_KEY=2c4bd1548a64141230c3f045d01921c8
 export SECRET_KEY=Annalisisawesome
 
-export MAIL_USERNAME=<Your Email Address>
-export MAIL_PASSWORD=<Your Email Password>
+export MAIL_USERNAME=Annalis
+export MAIL_PASSWORD=Annalis
 python3.6 manage.py server
